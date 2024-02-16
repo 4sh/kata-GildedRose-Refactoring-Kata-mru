0446be90043d8903d66818db3b397208bf391868
timothearroyo <timothe.arroyo@4sh.fr>
2024-02-16 10:53:54 +0100

    TAO

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,53 +8,49 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         items.forEach { item ->
-            if (item.name != BRIE && item.name != BACKSTAGE) {
-                if (item.quality > 0) {
-                    if (item.name != SULFURAS) {
+            if (item.name != SULFURAS) {
+                if (item.name in listOf(BRIE, BACKSTAGE)) {
+                    if (item.quality < 50) {
+                        item.quality += 1
+
+                        if (item.name == BACKSTAGE) {
+                            if (item.sellIn < 11) {
+                                addQuality(item, 1)
+                            }
+
+                            if (item.sellIn < 6) {
+                                addQuality(item, 1)
+                            }
+                        }
+                    }
+                } else {
+                    if (item.quality > 0) {
                         item.quality -= 1
                     }
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality += 1
-
-                    if (item.name == BACKSTAGE) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.name != SULFURAS) {
                 item.sellIn -= 1
-            }
 
-            if (item.sellIn < 0) {
-                if (item.name != BRIE) {
-                    if (item.name != BACKSTAGE) {
-                        if (item.quality > 0) {
-                            if (item.name != SULFURAS) {
+
+                if (item.sellIn < 0) {
+                    if (item.name == BRIE) {
+                        addQuality(item, 1)
+                    } else {
+                        if (item.name != BACKSTAGE) {
+                            if (item.quality > 0) {
                                 item.quality -= 1
                             }
+                        } else {
+                            item.quality -= item.quality
                         }
-                    } else {
-                        item.quality -= item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality += 1
                     }
                 }
             }
+        }
+    }
+
+    private fun addQuality(item: Item, value: Int) {
+        if (item.quality < 50) {
+            item.quality += value
         }
     }
 
