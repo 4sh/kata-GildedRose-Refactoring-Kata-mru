688708f6118619606a78cdc921d3bad10ea8e123
timothearroyo <timothe.arroyo@4sh.fr>
2024-03-20 14:28:52 +0100

    TAO

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -9,32 +9,46 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         items.forEach { item ->
 
-            if (item.name != SULFURAS) {
-                when {
-                    item.name == BACKSTAGE && item.sellIn < 6 -> {
-                        item.addQuality(3)
-                    }
+            when (item.name) {
+                SULFURAS -> {
 
-                    item.name == BACKSTAGE && item.sellIn < 11 -> {
-                        item.addQuality(2)
-                    }
-
-                    item.name in listOf(BACKSTAGE, BRIE) -> item.addQuality(1)
-                    else -> item.addQuality(-1)
                 }
+                else -> {
+                    updateQualityBeforeSellIn(item)
 
-                item.sellIn -= 1
+                    item.sellIn -= 1
 
-                if (item.sellIn < 0) {
-                    if (item.name == BRIE) {
-                        item.addQuality(1)
-                    } else {
-                        if (item.name != BACKSTAGE) {
-                            item.addQuality(-1)
-                        } else {
-                            item.addQuality(-item.quality)
-                        }
-                    }
+                    updateQualityAfterSellIn(item)
+                }
+            }
+        }
+    }
+
+    private fun updateQualityBeforeSellIn(item: Item) {
+        when (item.name) {
+            BACKSTAGE -> {
+                if (item.sellIn < 6) {
+                    item.addQuality(3)
+                } else if (item.sellIn < 11) {
+                    item.addQuality(2)
+                } else {
+                    item.addQuality(1)
+                }
+            }
+            BRIE -> item.addQuality(1)
+            else -> item.addQuality(-1)
+        }
+    }
+
+    private fun updateQualityAfterSellIn(item: Item) {
+        if (item.sellIn < 0) {
+            if (item.name == BRIE) {
+                item.addQuality(1)
+            } else {
+                if (item.name != BACKSTAGE) {
+                    item.addQuality(-1)
+                } else {
+                    item.addQuality(-item.quality)
                 }
             }
         }
